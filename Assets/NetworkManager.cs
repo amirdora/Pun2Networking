cd91d80d89d24baae4fce30600201564150d2603
AmirDora <amirdora@msn.com>
2021-01-20 14:40:58 +0100

    initial commit

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    private void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Play()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+       Debug.Log("Tried to join a room and failed");
+       // most likely because there is no room
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
